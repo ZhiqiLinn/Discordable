0e31df7114a090f46a4cb41fa404ec93f74e3377
zhiqilin <zhiqilinn@gmail.com>
2022-08-02 13:11:18 -0400

    server, channel, msg redux done

---
+++ react-app/src/store/channel.js
@@ -0,0 +1,148 @@
+// import { csrfFetch } from "./csrf";
+
+const ADD_CHANNEL = "channel/ADD_CHANNEL"
+const GET_CHANNEL = "channel/GET_CHANNEL"
+const GET_ALL_CHANNELS = "channel/GET_ALL_CHANNELS"
+const EDIT_CHANNEL = "channel/EDIT_CHANNEL"
+const DELETE_CHANNEL = "channel/DELETE_CHANNEL"
+
+//-------------------------action creator-------------------------
+const addChannel = (addedChannel) => {
+    return {
+        type: ADD_CHANNEL,
+        addedChannel
+    }
+}
+
+const getChannel = (channel) => {
+    return {
+        type: GET_CHANNEL,
+        channel
+    }
+}
+
+const getAllChannels = (allChannels) => {
+    return {
+        type: GET_ALL_CHANNELS,
+        allChannels
+    }
+}
+
+const editChannel = (editedChannel) => {
+    return {
+        type: EDIT_CHANNEL,
+        editedChannel
+    }
+}
+
+const deleteChannel = (deletedChannelId) => {
+    return {
+        type: DELETE_CHANNEL,
+        deletedChannelId
+    }
+}
+
+//-------------------------THUNK----------------------------------
+export const getAllChannelsThunk = () => async (dispatch) => {
+    const response = await fetch(`/api/channels`);
+    if (response.ok) {
+        const channels = await response.json();
+        console.log("GETALLCHANNELSTHUNK",channels)
+        dispatch(getAllChannels(channels));
+    }
+}
+
+export const addChannelThunk = (channel) => async (dispatch) => {
+    // const { user_id, name, description, category_id, address, city, state, zip_code, phone, website, price_range, channel_hours, latitude, longitude } = channel;
+    const response = await fetch(`/api/channels`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(channel),
+    });
+
+    if (response.ok) {
+        const newChannel = await response.json();
+        console.log("ADDCHANNELTHUNK",newChannel)
+
+        dispatch(addChannel(newChannel));
+        return newChannel;
+    }
+}
+
+
+
+
+export const getChannelThunk = (channelId) => async (dispatch) => {
+    const response = await fetch(`/api/channels/${channelId}`);
+
+    if (response.ok) {
+        const channel = await response.json();
+        console.log("GETCHANNELTHUNK",channel)
+
+        dispatch(getChannel(channel));
+    }
+}
+
+
+export const editChannelThunk = (channel) => async (dispatch) => {
+    const response = await fetch(`/api/channels/${channel.id}`, {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(channel)
+    })
+    if (response.ok) {
+        const newChannel = await response.json();
+        console.log("EDITEDCHANNELTHUNK",newChannel)
+        dispatch(editChannel(newChannel));
+        return newChannel;
+    }
+}
+
+
+export const deleteChannelThunk = (channelId) => async (dispatch) => {
+    const response = await fetch(`/api/channels/${channelId}`, {
+        method: 'DELETE'
+    });
+    if (response.ok) {
+        dispatch(deleteChannel(channelId));
+    }
+}
+//-------------------------reducer--------------------------------
+
+const initialState = {};
+
+const channelReducer = (state = initialState, action) => {
+    let newState;
+    switch (action.type){
+        case GET_CHANNEL:
+            newState = { ...state };
+            newState[action.channel.id] = action.channel;
+            return newState;
+        case GET_ALL_CHANNELS:
+            newState = {};
+            action.allChannels.Channel.forEach(ser => {
+                newState[ser.id] = ser;
+            });
+            return newState;
+        case ADD_CHANNEL:
+            return {
+                ...state,
+                [action.addedChannel.id]: action.addedChannel
+            };
+        case EDIT_CHANNEL:
+            return {
+                ...state,
+                [action.editedChannel.id]: action.editedChannel
+            };
+        case DELETE_CHANNEL:
+            newState = { ...state }
+            delete newState[action.deletedChannelId];
+            return newState;
+        default:
+            return state;
+    }
+}
+
+export default channelReducer;
---
+++ react-app/src/store/messages.js
@@ -0,0 +1,148 @@
+// import { csrfFetch } from "./csrf";
+
+const ADD_MESSAGE = "message/ADD_MESSAGE"
+const GET_MESSAGE = "message/GET_MESSAGE"
+const GET_ALL_MESSAGES = "message/GET_ALL_MESSAGES"
+const EDIT_MESSAGE = "message/EDIT_MESSAGE"
+const DELETE_MESSAGE = "message/DELETE_MESSAGE"
+
+//-------------------------action creator-------------------------
+const addMessage = (addedMessage) => {
+    return {
+        type: ADD_MESSAGE,
+        addedMessage
+    }
+}
+
+const getMessage = (message) => {
+    return {
+        type: GET_MESSAGE,
+        message
+    }
+}
+
+const getAllMessages = (allMessages) => {
+    return {
+        type: GET_ALL_MESSAGES,
+        allMessages
+    }
+}
+
+const editMessage = (editedMessage) => {
+    return {
+        type: EDIT_MESSAGE,
+        editedMessage
+    }
+}
+
+const deleteMessage = (deletedMessageId) => {
+    return {
+        type: DELETE_MESSAGE,
+        deletedMessageId
+    }
+}
+
+//-------------------------THUNK----------------------------------
+export const getAllMessagesForChannelThunk = (channelId) => async (dispatch) => {
+    const response = await fetch(`/api/messages/chan/${channelId}`);
+    if (response.ok) {
+        const messages = await response.json();
+        console.log("GETALLMESSAGESTHUNK",messages)
+        dispatch(getAllMessages(messages));
+    }
+}
+
+export const addMessageThunk = (message) => async (dispatch) => {
+    // const { user_id, name, description, category_id, address, city, state, zip_code, phone, website, price_range, message_hours, latitude, longitude } = message;
+    const response = await fetch(`/api/messages`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(message),
+    });
+
+    if (response.ok) {
+        const newMessage = await response.json();
+        console.log("ADDMESSAGETHUNK",newMessage)
+
+        dispatch(addMessage(newMessage));
+        return newMessage;
+    }
+}
+
+
+
+
+export const getMessageThunk = (messageId) => async (dispatch) => {
+    const response = await fetch(`/api/messages/${messageId}`);
+
+    if (response.ok) {
+        const message = await response.json();
+        console.log("GETMESSAGETHUNK",message)
+
+        dispatch(getMessage(message));
+    }
+}
+
+
+export const editMessageThunk = (message) => async (dispatch) => {
+    const response = await fetch(`/api/messages/${message.id}`, {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(message)
+    })
+    if (response.ok) {
+        const newMessage = await response.json();
+        console.log("EDITEDMESSAGETHUNK",newMessage)
+        dispatch(editMessage(newMessage));
+        return newMessage;
+    }
+}
+
+
+export const deleteMessageThunk = (messageId) => async (dispatch) => {
+    const response = await fetch(`/api/messages/${messageId}`, {
+        method: 'DELETE'
+    });
+    if (response.ok) {
+        dispatch(deleteMessage(messageId));
+    }
+}
+//-------------------------reducer--------------------------------
+
+const initialState = {};
+
+const messageReducer = (state = initialState, action) => {
+    let newState;
+    switch (action.type){
+        case GET_MESSAGE:
+            newState = { ...state };
+            newState[action.message.id] = action.message;
+            return newState;
+        case GET_ALL_MESSAGES:
+            newState = {};
+            action.allMessages.ChannelMessage.forEach(ser => {
+                newState[ser.id] = ser;
+            });
+            return newState;
+        case ADD_MESSAGE:
+            return {
+                ...state,
+                [action.addedMessage.id]: action.addedMessage
+            };
+        case EDIT_MESSAGE:
+            return {
+                ...state,
+                [action.editedMessage.id]: action.editedMessage
+            };
+        case DELETE_MESSAGE:
+            newState = { ...state }
+            delete newState[action.deletedMessageId];
+            return newState;
+        default:
+            return state;
+    }
+}
+
+export default messageReducer;
